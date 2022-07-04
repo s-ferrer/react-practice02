ae746753f48e2d8001b86e091295bdd3f6fc6772
s-ferrer <saraferrer@gmail.com>
2022-07-04 12:33:19 +0200

    Add images for Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function Card() {
-  return <h1>Replace this</h1>;
+  return (
+    <div className="card">
+      <img
+        src="../images/person-home-01.jpg"
+        alt="person at home"
+        className="card-img"
+      />
+      <div className="card-stats">
+        <img src="../images/star.png" alt="star icon" className="card-star" />
+        <span className="gray">5.0</span>
+        <span className="gray">(6) . </span>
+        <span>USA</span>
+      </div>
+      <p>Life Lessons with Katie</p>
+      <p>From 200€ / person</p>
+    </div>
+  );
 }
