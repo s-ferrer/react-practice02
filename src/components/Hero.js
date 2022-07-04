3e76078d603f1a3da322bad4f92bd22dbaa690cb
s-ferrer <saraferrer@gmail.com>
2022-07-04 12:02:33 +0200

    Fisnish Hero component and Create Card component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,18 +3,16 @@ import heroimage from "../images/hero-img.jpg";
 
 export default function Hero() {
   return (
-    <div className="container">
+    <section className="hero">
       <div>
         <img src={heroimage} className="hero-img" alt="hero" />
       </div>
 
-      <div className="text-hero">
-        <h1>Online Experiences</h1>
-        <p>
-          Join unique interactive activities led by one-of-a-kind hosts—all
-          without leaving home.
-        </p>
-      </div>
-    </div>
+      <h1 className="hero-header">Online Experiences</h1>
+      <p className="hero-text">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
   );
 }
