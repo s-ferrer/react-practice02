cfc1c3546d7a4ffea23a4bc2b81666b41dd23663
s-ferrer <saraferrer@gmail.com>
2022-07-04 16:50:20 +0200

    Remove contact in App.js

--- src/App.js
+++ src/App.js
@@ -2,37 +2,35 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-import Contact from "./components/Contact";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card />
-      <Contact
-        img="./images/mr-whiskerson.png"
-        name="Mr. Whiskerson"
-        phone="(212) 555-1234"
-        email="mr.whiskaz@catnap.meow"
+      <Card
+        img="./images/person-home-01.jpg"
+        rating="5.0"
+        reviewCount={6}
+        country="Whatever you want"
+        title="Life Lessons with Katie ZaferesRepair "
+        price={136}
       />
-      <Contact
-        img="./images/mr-whiskerson.png"
-        name="Fluffykins"
-        phone="(88) 555-9928284"
-        email="fjlkhj@catnap.meow"
+      <Card
+        img="./images/person-home-02.jpg"
+        rating="3.0"
+        reviewCount={16}
+        country="Whatever you want"
+        title="Life Lessons with Katie Zaferes"
+        price={89}
       />
-      <Contact
-        img="./images/mr-whiskerson.png"
-        name="Felix"
-        phone="(562) 87067823"
-        email="felix@catnap.meow"
-      />
-      <Contact
-        img="./images/mr-whiskerson.png"
-        name="Pumpkin"
-        phone="(232) 76798159"
-        email="pump@catnap.meow"
+      <Card
+        img="./images/person-home-03.jpg"
+        rating="4.0"
+        reviewCount={8}
+        country="Whatever you want"
+        title="Life Lessons with Katie Zaferes"
+        price={250}
       />
     </div>
   );
